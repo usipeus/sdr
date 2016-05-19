bb18f1e1805ca3339bc585695b34a59706383abf
Young Li <li.young1618@gmail.com>
2016-05-19 12:36:47 -0700

    Add comments and improve docstring

--- sdr.py
+++ sdr.py
@@ -5,11 +5,18 @@ import string
 
 def roll_dice(n, s):
     ''' Returns a list with the results of rolling n dice,
-    each dice with s sides; the sum is the last list element '''
+    each dice with s sides; the sum is the last list element
+
+    for example, if n = 2 and s = 6, roll_dice calculates the
+    sum of two rolls of a six-sided die '''
+
+    # error checks
     assert abs(n) > 0, "not allowed to roll 0 dice!"
     assert abs(s) > 1, "can't roll 0 or 1 sided dice!"
 
+    # list that holds the results of the dice roll, currently isn't returned
     results = []
+    # the roll's total value
     total = 0
 
     for i in range(abs(n)):
@@ -17,6 +24,7 @@ def roll_dice(n, s):
         results.append(current)
         total += current
 
+    # used to interpret subtraction e.g. if the user inputs 1d20 - 2d6
     if n < 0:
         total = -total
 
@@ -25,7 +33,9 @@ def roll_dice(n, s):
     return total
 
 def parse_roll(roll):
-    ''' Returns a list of all the elements in the roll '''
+    ''' Returns a list of all the elements in the roll string, which is
+    comprised of dice rolls (e.g. 2d20) and modifiers (e.g. 1, -5, 13) joined
+    by + or - '''
 
     # separate into parts (divided by + or -)
     roll = roll.replace('-', '+-')
@@ -47,9 +57,11 @@ def parse_roll(roll):
 
     for i in range(len(parts)):
         if type(parts[i]) is int:
+            # the list element is a modifier, so just add it to the total
             result += str(parts[i])
             total += parts[i]
         else:
+            # the list element is a dice roll
             current_roll = roll_dice(parts[i][0], parts[i][1])
             result += "{" + str(parts[i][0]) + "d" + str(parts[i][1])
             result += ": " + str(current_roll) + "}"
